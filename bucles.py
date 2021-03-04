be6565d967f64de902db90ddf4e1b3637a7b64bb
juan camilo <juanca2.0@hotmail.com>
2021-03-03 21:36:49 -0500

    cliclo while

---
+++ bucles.py
@@ -0,0 +1,27 @@
+# def potencia(num,num2,pot):
+#     if(num <= pot):
+#         cont = num
+#         print(str(num2 ** cont))
+#         potencia(num+1,num2, pot)
+#     else:
+#         print('End')
+
+# if __name__ == '__main__':
+#     num2 = int(input('ingresa numero que le quieres sacar la potencia: '))
+#     pot = int(input('cantidad de potencias: '))
+#     potencia(0, num2, pot)
+
+# ciclo while  
+# def run():
+#     LIMITE = 10000000
+    
+#     contador = 0
+#     potecia_2 = 2**contador
+#     while potecia_2 < LIMITE:
+#         print(f'2 elevado a {contador} es igual a {potecia_2}')
+#         contador += 1
+#         potecia_2 = 2**contador
+
+# if __name__ == '__main__':
+
+#     run()
